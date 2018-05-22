146131aee23f1d1c845d326bb3c01b368ed40ea3
CyberHazard <cyberhazard@inbox.ru>
2018-05-22 23:14:02 +0300

    fix space between slides, change speed

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,8 @@ void function() {
   const slider = document.querySelector('.i-Slider__slider');
   if (!slider) return null;
   new Swiper(slider, {
-    spaceBetween: 100,
+    spaceBetween: 300,
+    speed: 700,
     navigation: {
       nextEl: '.i-Slider__button_right',
       prevEl: '.i-Slider__button_left',
